c368c3aa6b065f6fc193d0863f8a28182d337594
AO <aolgu015@ucr.edu>
2025-02-20 22:54:12 -0800

    Started Products page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext';
 
 const Product = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Product
+  const {productID} = useParams();
+  const {products, currency} = useContext(ShopContext);
+  const [productData, setProductData] = useState(false);
+  const [image, setImage] = useState('');
+  const [size,setSize] = useState('')
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productID) {
+        setProductData(item)
+        setImage(item.image[0])
+        console.log(item);
+        return null;
+      }
+    })
+  }
+
+    <div className=' border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-10'>
+      { /* ------- Product Data ------- */ }
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        { /* ------- Product Images ------- */ }
+        <div className=' flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className=' flex sm:flex-col overflow-x-auto sm: overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+            {
+              productData.image.map((item,index)=>(
+                <img onClick={()=> setImage(item)} src={item} key={index} classname='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' alt="" />
+              ))
+            }
+          </div>
+          <div className='w-full sm:w-[80%]'>
+            <img className=' w-full h-auto' src={image} alt="" /> 
+          </div>
+        </div>
+        { /* ------- Product Info ------- */ }
+        <div className='flex-1'>
+            <h1 className='front-medium text-2xl mt-2'>{productData.name} </h1>
+            <div className=' flex items-center gap-1 mt-2'>
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_icon} alt="" className="w-3 5" />
+              <img src={assets.star_dull_icon} alt="" className="w-3 5" />
+              <p className='pt-2'>(122)</p>
+            </div>
+            <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
+            <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+            <div className='flex flex-col gap-4 my-8'>
+              <p>SelectSize</p>
+              <div className='flex gap-2'>
+                {productData.sizes.map((item,index) => (
+                  <button key={index} onClick={() => setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : ''}`}>{item}</button>
+
+                ))}
+              </div>
+            </div>
+            <button className='bg-black text-white' px-8 py-3 text-sm active:bg-gray-700> ADD TO CART </button>
+            <hr className=' mt-8 sm:w-4/5' /> 
+            <div> className='text-sm text-gray-500 mt-5 flex flex-col gap-1' </div>
+                  <p> 100% Original Product. </p>
+                  <p> Cash on delivery is available on this product. </p>
+                  <p> Easy return and exchange policy within 7 days. </p>
+        </div>
+      </div>
+    </div>
+      { /* ------- Description & Review Section ------- */ }
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description </b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122) </p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+          <p> DUMMY TEXT // FIX ME </p>
+          <p> DUMMY TEXT // FIX ME TOO </p>
+        </div>
+      </div>
+
+    { /* ------- Description & Review Section ------- */ }
+
+  useEffect(()=>{
+    fetchProductData();
+  }, [productID])      
+
+  return productData ? (
+
+    <div>
+
+    </div>
+  ) : <div className=' opacity-0' ></div>
+
+export default Product;
+
+}
