8fce8e5feea84ca813b853bfb028a50e8373c893
Jashan Mann <jashan@Jashans-MBP-2.lan>
2025-02-21 23:34:36 -0800

    adding edits for NavBar and ShopContext for collections page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { assets } from "../assets/assets";
 import { NavLink, Link } from 'react-router-dom';
+import { ShopContext } from '../context/ShopContext';
 
 
 const Navbar = () => {
  const [visible, setVisible] = useState(false);
-
+ const {setShowSearch} = useContext(ShopContext);
 
  return (
    <div className='flex items-center justify-between py-5 font-medium'>
@@ -33,7 +34,7 @@ const Navbar = () => {
 
 
      <div className='flex items-center gap-6'>
-       <img src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
+       <img onClick={()=> setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
 
 
        <div className='group relative'>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -6,9 +6,12 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = '$';
   const delivery_fee = 10;
+  const [search, setSearch] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
 
   const value = {
-    products, currency, delivery_fee
+    products, currency, delivery_fee,
+    search, setSearch, showSearch, setShowSearch
   };
 
   return (
