50daed32444e465e135e2cd5602e172ae22c6dc9
sahithimatta27 <146261501+sahithimatta27@users.noreply.github.com>
2025-02-21 19:35:04 -0800

    modified best seller sahithi

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -16,6 +16,18 @@ const BestSeller = () => {
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum.
                 </p>
             </div>
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+                {bestSellers.map((item, index) => (
+                    <ProductItem 
+                        key={index} 
+                        id={item._id} 
+                        name={item.name} 
+                        image={item.image} 
+                        price={item.price} 
+                    />
+                ))}
+            </div>
         </div>
     );
 };
