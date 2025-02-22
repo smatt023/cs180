e96376cf612011259ed1c2ef0204b6a64fcfb004
Bhavna Ramkumar <bhavna@Bhavnas-MBP-2.lan>
2025-02-21 20:23:53 -0800

    sahithi latestcollection

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,30 +1,31 @@
-import React, { useContext } from 'react'
-import {ShopContext} from '../context/ShopContext'
+import { useEffect, useState } from "react";
 
 const LatestCollection = () => {
+    const [latestProducts, setLatestProducts] = useState([]);
 
-    const { products } = useContext(ShopContext);
-    //console.log(products);
-
-    const[latestProducts, setLatestProducts] = useState([])
-
-    useEffect(()=>{
-        setLatestProducts(products.slice(0,10));
-    },[]
-    
-    )
+    useEffect(() => {
+        setLatestProducts(products.slice(0, 10));
+    }, []);
 
     return (
-    <div className='my-10'>
-        <div className='text-center py-8 tezt-3xl'>
-            <Title text1 = {'LATEST'} text2= {'COLLECTIONS'} />
-            <p className='w-3/4 m- auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Texttttt</p>
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                </p>
+            </div>
 
+            {/* Rendering Products */}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+                {
+                    latestProducts.map((item, index) => (
+                        <ProductItem key={index} />
+                    ))
+                }
+            </div>
         </div>
-      
-    </div>
-  )
-}
+    );
+};
 
-export default Navbar
+export default LatestCollection;
